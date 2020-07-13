47bbe275cdd9fdff0fb282f40f9f6672cbd96b0d
B Dog <bluedog@speedmail.se>
2020-07-13 13:00:57 +0200

    Band

--- app.go
+++ app.go
@@ -7,18 +7,20 @@ import (
 	"github.com/go-rod/rod"
 )
 
+const (
+	row  string = ".flex.items-center.bb.pb2.b--light-gray"
+	band string = ".link.outline-0.source-sans-b.black.w-40.pr3.h2.flex.items-center"
+
+	// salary float64 = 50000.0
+)
+
 func main() {
-	// Launch a new browser with default options, and connect to it.
 	browser := rod.New().Connect()
 
-	// Even you forget to close, rod will close it after main process ends.
 	defer browser.Close()
 
-	// Timeout will be passed to all chained function calls.
-	// The code will panic out if any chained call is used after the timeout.
 	page := browser.Timeout(time.Minute).Page("https://www.fender.com/play/guitar/artists/songs")
 
-	// Resize the window make sure window size is always consistent.
 	page.Window(0, 0, 1200, 600)
 
 	fmt.Println("done1")
@@ -27,11 +29,13 @@ func main() {
 	// band
 	// text := page.Element(".link.outline-0.source-sans-b.black.w-40.pr3.h2.flex.items-center").Text()
 	// song
-	// text := page.Element(".black.db.w-40.pr2.source-sans.ttc.h2.flex.items-center").Text()
-	// runtom
-	text := page.Element(".flex.items-center.bb.pb2.b--light-gray").HTML()
 
-	fmt.Println(text)
+	text := page.Element(row).Text()
+	fmt.Printf("Hello %s\n", text)
 
-	// Output: Git is the most widely used version control system.
+	for i, e := range page.Elements(row) {
+		fmt.Printf("%d \n", i)
+		temp := e.Element(band).Text()
+		fmt.Printf("%d %s\n", i, temp)
+	}
 }
